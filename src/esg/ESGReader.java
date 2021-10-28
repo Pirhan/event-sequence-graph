95850f8fc58b18e04e87881f902c3a0280ccbc37
Mert Anil Hasret <mertanilhasret@hotmail.com>
2021-10-28 02:26:36 +0200

    Removed unused import statements

--- src/esg/ESGReader.java
+++ src/esg/ESGReader.java
@@ -1,11 +1,7 @@
 package esg;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.util.HashMap;
